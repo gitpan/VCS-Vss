c56e12368542ac3fa9574d3143a867049d1ce35f
James Tillman <jtillman@bigfoot.com>
2003-03-07 12:15:31 +0000

    Import of JTILLMAN/VCS-Vss-0.10 from CPAN.
    gitpan-cpan-distribution: VCS-Vss
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         JTILLMAN/VCS-Vss-0.10.tar.gz
    gitpan-cpan-author:       JTILLMAN
    gitpan-cpan-maturity:     released

---
+++ Vss/File.pm
@@ -0,0 +1,27 @@
+package VCS::Vss::File;
+
+@ISA = qw(VCS::Vss VCS::File);
+
+use VCS::Vss;
+use Carp;
+
+use strict;
+
+sub new {
+    my($class, $url) = @_;
+    my $self = $class->init($url);
+	$self->_fix_path;
+	#$self->{vss_object} = $self->_get_vss_item($self->path);
+    return $self;
+}
+
+# evil assumption - no query strings on URL!
+sub versions {
+    my ($self, $lastflag) = @_;
+	my $current_version = $self->vss_object->VersionNumber;
+    map {
+        VCS::Vss::Version->new("$self->{URL}/$_")
+    } (1..$current_version);
+}
+
+1;
